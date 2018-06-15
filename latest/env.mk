6e9932859183deaebb20149bc4a088f2fa5a2b9a
Maël Le Garrec <mlegarrec@online.net>
2018-06-15 15:13:05 +0200

    Initial commit
    FreshRSS installation OK

---
+++ latest/env.mk
@@ -0,0 +1,6 @@
+IMAGE_NAME = freshrss
+IMAGE_VERSION = latest
+IMAGE_TITLE = freshrss
+IMAGE_DESCRIPTION = "FreshRSS is a self-hosted RSS feed aggregator such as Leed or Kriss Feed."
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-freshrss
+IMAGE_VENDOR_URL = https://freshrss.org/
